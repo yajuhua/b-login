ce8b0745f56c24cdcc39234d083ec38b2806a67d
yajuhua <yajuhua@outlook.com>
2024-08-03 15:30:28 +0800

    [update]添加heades

--- api/checkLogin.js
+++ api/checkLogin.js
@@ -5,7 +5,14 @@ export default async (req, res) => {
     const url = `https://passport.bilibili.com/x/passport-login/web/qrcode/poll?qrcode_key=${qrcode_key}`;
 
     try {
-        const response = await fetch(url);
+        const response = await fetch(url,{
+            method: 'GET', // 你也可以使用 POST 或其他 HTTP 方法
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
+                Origin: 'https://www.bilibili.com',
+                Referer: 'https://www.bilibili.com/',
+            }
+        });
 
         // 从响应头中获取 Set-Cookie
         const cookies = response.headers.get('set-cookie');
--- api/generateQrcode.js
+++ api/generateQrcode.js
@@ -1,10 +1,17 @@
 // api/generateQrcode.js
 
 export default async (req, res) => {
-    const url = 'https://passport.bilibili.com/x/passport-login/web/qrcode/generate';
+    const url = 'https://passport.bilibili.com/x/passport-login/web/qrcode/generate?source=main-fe-header';
 
     try {
-        const response = await fetch(url);
+        const response = await fetch(url,{
+            method: 'GET', // 你也可以使用 POST 或其他 HTTP 方法
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
+                Origin: 'https://www.bilibili.com',
+                Referer: 'https://www.bilibili.com/',
+            }
+        });
         const data = await response.json();
 
         res.status(200).json(data);
